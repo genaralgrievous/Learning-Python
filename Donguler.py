63ea080f3249fe15c26841d49e1762ed8b048951
CanAydogan <140336507+genaralgrievous@users.noreply.github.com>
2025-10-06 14:55:23 +0200

    Donguler gun 1

---
+++ Donguler.py
@@ -0,0 +1,29 @@
+x=7
+y=8
+not x != y
+# Degerler birbirine esit mi degil mi kontrol etmek icin" == "ifadesini kullanabiliriz .
+
+x != y
+# veya esit degil mi diye sormak icin " != " ifadesini kullanabiliriz
+
+my_list = [10,20,30]
+10 in my_list
+#in kullanarak veya not in ile aradigimiz deger listenin icinde mi diye de kontrol edebiliriz
+
+
+###################-------------####################
+
+my_age = 20
+
+if my_age < 18 :
+    print("your age is < 18")
+elif my_age > 18 and my_age <= 30 :
+    print("you are a young man")
+elif my_age > 30 and my_age < 45:
+    print("you are middle aged man")
+else:
+    my_age > 45
+    print("now you are getting older a little bit")
+# elif ve else lerden sonra indention birakilmaz ayrica elif ve if 'in aksine else kullanirken kosulu dongunun
+#ilk satirina degil altina yazilir
+
