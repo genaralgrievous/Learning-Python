88674248ef9eb5997477205fde885f385a29da6b
CanAydogan <140336507+genaralgrievous@users.noreply.github.com>
2025-10-22 12:40:02 +0200

    Day 11

---
+++ module1/subModule/subModuleDosyasi.py
@@ -0,0 +1,15 @@
+def subModuleDosyasi():
+    print("subModuleDosyasi calistirildi")\
+
+
+def speakDirect():
+    print("submodule directed")
+
+def speakImported():
+    print("subModule imported")
+
+if __name__== "__main__":
+    print("subModule direct")
+
+else :
+    print("subModule imported")
